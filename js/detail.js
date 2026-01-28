4e3cc66baac65371f601a7b25c4672458b7b8921
Georgette237 <georgettekaldjob@gmail.com>
2026-01-28 01:31:30 +0100

    Delete js/detail.js

--- js/detail.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const card = document.querySelector(".domain-card");
-
-    if (card) {
-        card.addEventListener("click", () => {
-            window.location.href = "detail.html";
-        });
-    }
-});
